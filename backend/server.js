e2cd66abe11fe7b279ec39b926a27a9f0bc3a82f
Dean MacLanders <dean@automac.ca>
2025-08-27 10:08:55 -0700

    troublshooting build

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ if (process.env.NODE_ENV === 'production') {
 
     // For any request that doesn't match an API route, send back React's index.html file.
     // This is the key for single-page applications to work with client-side routing.
-    app.get('*', (req, res) => {
+    app.get('/{*any}', (req, res) => {
         res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
     });
 }
