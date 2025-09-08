48247db63e7ba040f7c732dd9414d88a4ac130ca
Dean MacLanders <dean@automac.ca>
2025-09-07 18:37:05 -0700

    troubleshoot deployment

--- backend/database.js
+++ backend/database.js
@@ -21,12 +21,21 @@ if (isProduction) {
   };
 }
 
-sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+sequelize = new Sequelize(process.env.DATABASE_URL || {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: 'postgres',
   logging: isProduction ? false : console.log,
-  dialectOptions,
+  dialectOptions: isProduction ? {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Temporarily set to false for testing
+      ca: process.env.CA_CERT
+    }
+  } : {}
 });
 
 // In database.js, after creating the sequelize instance
